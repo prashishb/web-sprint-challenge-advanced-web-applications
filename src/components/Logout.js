94499d6b3a396a2b506ba119b662174a92ef4600
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-11-12 11:10:41 -0500

    logout authentication complete

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
-const Logout = () => {        
-    return(<div></div>);
-}
+const Logout = (props) => {
+    useEffect(() => {
+      axiosWithAuth()
+        .post('/logout')
+        .then((res) => {
+          localStorage.removeItem('token');
+          props.history.push('/login');
+        });
+    }, []);
+    return <div></div>;
+  };
 
 export default Logout;
 
